b3873ca80b3bb2ccfb66915c889a9f8228dbd4fb
danoff <denis.danov@dreamix.eu>
2023-05-30 14:41:29 +0300

    start adding IAC

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import { config } from './src/config.js';
 import { errorHandler } from './src/application/http/errorHandler.js';
 import { createPublicRouter } from './src/application/http/routes/public.js';
 import { createPrivateRouter } from './src/application/http/routes/private.js';
+import { createUiRouter } from './src/application/http/routes/ui.js';
 
 const start = async () => {
     const app = express();
@@ -13,13 +14,12 @@ const start = async () => {
     app.use(ipFilter(config.web.blockedIps));
     app.use('/api', createPublicRouter());
     app.use('/api/private', createPrivateRouter());
+    app.use(createUiRouter());
     app.use(errorHandler(logger));
     app.listen({ port: config.web.httpPort }, () => {
         logger.info({
-            msg: `🚀 Server ready to server public API at: http://localhost:${config.web.httpPort}/api`,
-        });
-        logger.info({
-            msg: `🚀 Server ready to server private API at: http://localhost:${config.web.httpPort}/api/private`,
+            msg: `🚀 Server ready to server public API at: http://localhost:${config.web.httpPort}/api 
+            and private API at: http://localhost:${config.web.httpPort}/api/private`,
         });
     });
 };
--- backend/src/application/http/routes/ui.js
+++ backend/src/application/http/routes/ui.js
@@ -2,14 +2,15 @@ import path from 'path';
 import express from 'express';
 import { config } from '../../../config.js';
 
-export const createUiRouter = (contentDir) => {
+export const createUiRouter = () => {
     const uiRouter = express.Router();
     if (config.web.serveFrontend) {
+        const frontendDir = path.resolve(config.web.frontendPath);
         // Have Node serve the files for our built React app
-        uiRouter.use(express.static(contentDir));
+        uiRouter.use(express.static(frontendDir));
         // All other GET requests not handled before will return our React app
         uiRouter.get('*', (_req, res) => {
-            res.sendFile(path.join(contentDir, 'index.html'));
+            res.sendFile(path.join(frontendDir, 'index.html'));
         });
     }
 
